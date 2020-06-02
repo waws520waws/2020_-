84ff05c3018dbb2a296167d522d57f7a825de65d
waws520waws <16601203140@163.com>
2020-06-03 07:59:44 +0800

    6.3commit

--- easy_algorithms/广度优先搜索.py
+++ easy_algorithms/广度优先搜索.py
@@ -22,6 +22,10 @@ def person_is_seller(person):
     return person[-1] == "m"
 
 
+"""
+在这个版本中我们忽略了一个问题：就是我们的朋友中也可能有我们自己的存在，只是上面的图中并没有展示而已
+这样假设我们在我的朋友中还会再次将我添加到搜索的队列中，这样将会形成死循环，所以改进的建议就是
+增加一个列表，将搜索过的元素加入到这个列表中，以免再次对其进行搜索
 def find_mango_seller():
     search_queue = deque()
     search_queue += graph["you"]
@@ -33,7 +37,23 @@ def find_mango_seller():
         else:
             search_queue += graph[person]
     return False
+"""
+
+
+def find_mango_seller(name):
+    search_queue = deque()
+    search_queue += graph[name]
+    search_list = []
+    while search_queue:
+        person = search_queue.popleft()
+        if not person in search_list:
+            if person_is_seller(person):
+                print(person + " is a mango seller!")
+                return True
+            else:
+                search_queue += graph[person]
+    return False
 
 
 if __name__ == '__main__':
-    find_mango_seller()
+    find_mango_seller("you")
